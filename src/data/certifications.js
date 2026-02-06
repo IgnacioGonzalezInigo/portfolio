7af39a6b665ba3eb2d9899af90c070df0529ea20
ignaciogonzalezinigo1 <ignaciogonzalez1@uca.edu.ar>
2026-02-06 12:18:48 -0300

    routes fix

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -1,15 +1,15 @@
 export const certificationsData = [
     {
         title: "Web Development",
-        image: "/images/cert1.png"
+        image: "images/cert1.png"
     },
     {
         title: "Javascript",
-        image: "/images/cert2.png"
+        image: "images/cert2.png"
     },
     {
         title: "React JS",
-        image: "/images/cert3.png"
+        image: "images/cert3.png"
     },
     
 ]
